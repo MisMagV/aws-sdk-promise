ab4f4dbab876a56c3382c4d91d8d9306cb83e6d4
Jeffrey Jen <yihungjen@gmail.com>
2015-12-16 14:07:39 +0000

    UPDATE: s3 promise with extra debug

--- s3.js
+++ s3.js
@@ -1,6 +1,12 @@
 "use strict";
 
-var _ = require("lodash");
+var _ = require("lodash"),
+    util = require("util");
+
+var debug = console.log;
+if (util.debuglog !== undefined) {
+    debug = util.debuglog("s3-promise");
+}
 
 var aws = require("aws-sdk"),
     Promise = require("promise");
@@ -22,7 +28,7 @@ module.exports = function(option) {
             return new Promise(doCopyObject);
         },
 
-        GetObject: function(param, timeout) {
+        GetObject: function(param, extra) {
             function doGetObject(ok, grr) {
                 var getReq = s3.getObject(param)
                     .on("success", function(data) {
@@ -35,15 +41,20 @@ module.exports = function(option) {
                     .on("error", function(err) {
                         grr(err);
                     });
-                if (timeout !== undefined) {
+                if (extra && extra.timeout !== undefined) {
                     setTimeout(getReq.abort.bind(getReq), timeout);
+                }
+                if (extra && extra.debug === true) {
+                    getReq.on("httpDownloadProgress", function(progress) {
+                        debug("GetObject:", progress);
+                    });
                 }
                 getReq.send();
             }
             return new Promise(doGetObject);
         },
 
-        PutObject: function(param, timeout) {
+        PutObject: function(param, extra) {
             function doPutObject(ok, grr) {
                 var putReq = s3.putObject(param)
                     .on("success", function() {
@@ -52,25 +63,32 @@ module.exports = function(option) {
                     .on("error", function(err) {
                         grr(err);
                     });
-                if (timeout !== undefined) {
+                if (extra && extra.timeout !== undefined) {
                     setTimeout(putReq.abort.bind(putReq), timeout);
+                }
+                if (extra && extra.debug === true) {
+                    putReq.on("httpUploadProgress", function(progress) {
+                        debug("PutObject:", progress);
+                    });
                 }
                 putReq.send();
             }
             return new Promise(doPutObject);
         },
 
-        DeleteObjects: function(param) {
+        DeleteObjects: function(param, extra) {
             // best effort remove delete old video
             function doDeleteObjects(ok, grr) {
                 var delReq = s3.deleteObjects(param)
-                .on("success", function(data) {
-                    ok(param);
-                })
-                .on("error", function(err) {
-                    grr(err);
-                });
-
+                    .on("success", function(data) {
+                        ok(param);
+                    })
+                    .on("error", function(err) {
+                        grr(err);
+                    });
+                if (extra && extra.timeout !== undefined) {
+                    setTimeout(delReq.abort.bind(delReq), timeout);
+                }
                 delReq.send(); // fire away;
             }
             return new Promise(doDeleteObjects);
